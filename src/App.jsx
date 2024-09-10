77f6f11818126b88a26277bc575d1f1420c273a4
Mateo <mateo.avalos3942@gmail.com>
2024-09-10 15:33:44 -0500

    Added semicolon

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ function App() {
         </Routes>
       </Router>
     </div>
-  )
-}
+  );
+};
 
 export default App
--- src/Components/LogIn-SignUp/LogIn.jsx
+++ src/Components/LogIn-SignUp/LogIn.jsx
@@ -28,5 +28,5 @@ export const LogIn = () => {
                 </div>
             )}
         </div>
-    )
-}
+    );
+};

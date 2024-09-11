3c1b1887ed0d5f9e50388d4b580e10b9d4da0000
Mateo <mateo.avalos3942@gmail.com>
2024-09-11 08:52:50 -0500

    Added a row for hours

--- src/assets/ScheduleManagement/Schedule.jsx
+++ src/assets/ScheduleManagement/Schedule.jsx
@@ -8,6 +8,7 @@ export const Schedule = () => {
       <table className="schedule-table">
         <thead>
           <tr>
+            <th></th>
             <th>Lunes</th>
             <th>Martes</th>
             <th>Miércoles</th>
@@ -18,39 +19,11 @@ export const Schedule = () => {
         <tbody>
           {hours.map((hour) => (
             <tr key={hour}>
-              <td>{hour}:00</td>
+              <td >{hour}</td>
+              <td></td>
               <td></td>
               <td>
-                {hour === 12 ? (
-                  <div>
-                    <span>
-                      <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"/>
-                      </svg>
-                    </span>
-                  </div>
-                ) : (
-                  <div>
-                    {hour === 10 && (
-                      <div>
-                        <span>
-                          <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"/>
-                          </svg>
-                        </span>
-                      </div>
-                    )}
-                    {hour === 11 && (
-                      <div className="flex justify-center items-center">
-                        <span className="text-red-500">
-                          <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"/>
-                          </svg>
-                        </span>
-                      </div>
-                    )}
-                  </div>
-                )}
+                
               </td>
               <td></td>
               <td></td>
